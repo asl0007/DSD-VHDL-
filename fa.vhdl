25effd1784ce249ed529834e4e36b1a0785c45ef
Shiraz Ali <32427100+asl0007@users.noreply.github.com>
2019-03-15 01:19:44 +0530

    Update fa.vhdl

--- fa.vhdl
+++ fa.vhdl
@@ -7,7 +7,7 @@ entity fa is
 	sum,carry : out std_logic);
   end fa;
 
-architecture behave of gates is
+architecture behave of fa is
   begin	
 	sum <= a xor b xor c;
 	carry <= (a and b) or (b and c) or (c and a);
